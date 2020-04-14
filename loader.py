0c1b561b80882877a783362c08e83325033435ae
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-04-14 13:56:34 +0300

    Replaced logging to misc

--- loader.py
+++ loader.py
@@ -1,5 +1,3 @@
-import logging
-
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
@@ -8,7 +6,3 @@ from data import config
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = RedisStorage2(**config.aiogram_redis)
 dp = Dispatcher(bot, storage=storage)
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO,
-                    # level=logging.DEBUG,
-                    )
