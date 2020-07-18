3d9945ef8b362ab248e8756107b23ef44460afa7
0kit <kostyajog@mail.ru>
2020-07-18 15:57:58 +0300

    docstrings

--- data/permissions.py
+++ data/permissions.py
@@ -24,14 +24,7 @@ user_allowed = types.ChatPermissions(
     can_pin_messages=False,
 )
 
-# Права пользователя в муте
-user_ro = types.ChatPermissions(
-    can_send_messages=False,
-    can_send_media_messages=False,
-    can_send_polls=False,
-    can_send_other_messages=False,
-    can_add_web_page_previews=False,
-    can_invite_users=True,
-    can_change_info=False,
-    can_pin_messages=False,
-)
+__all__ = [
+    'new_user_added',
+    'user_allowed',
+]
